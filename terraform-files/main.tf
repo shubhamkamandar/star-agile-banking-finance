d5b01030e0ff2775fa70470928a5791b729154fd
Shubham Kamandar <93696457+shubhamkamandar@users.noreply.github.com>
2024-10-19 15:53:02 +0530

    Updated main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "test-server" {
-  ami = "ami-06b21ccaeff8cd686"
+  ami = "ami-09b23fgaesd8cd148"
   instance_type = "t2.micro"
-  key_name = "mykey"
-  vpc_security_group_ids = ["sg-04cf4e3801bd09203"]
+  key_name = "final-key"
+  vpc_security_group_ids = ["sg-04as4e1591bd05236"]
   connection {
      type = "ssh"
      user = "ec2-user"
@@ -19,6 +19,6 @@ resource "aws_instance" "test-server" {
      command = "echo ${aws_instance.test-server.public_ip} > inventory"
      }
   provisioner "local-exec" {
-     command = "ansible-playbook /var/lib/jenkins/workspace/BankingProject/terraform-files/ansibleplaybook.yml"
+     command = "ansible-playbook /var/lib/jenkins/workspace/Banking-Project/terraform-files/ansibleplaybook.yml"
      }
   }
